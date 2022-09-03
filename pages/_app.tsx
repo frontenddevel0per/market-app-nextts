d355b52176ecd5553cf8cf2cb8e705e534e9aefd
Leonid <leonovozhilov1967@gmail.com>
2022-09-03 18:35:52 +0300

    added redux-persist

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,17 @@ import "../styles/index.scss";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import store from "../redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+
+let persistor = persistStore(store);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <PersistGate loading={null} persistor={persistor}>
+        <Component {...pageProps} />
+      </PersistGate>
     </Provider>
   );
 }
--- redux/store.tsx
+++ redux/store.tsx
@@ -1,9 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
 import bagReducer from "./bag-slice";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, bagReducer);
 
 const store = configureStore({
   reducer: {
-    bag: bagReducer,
+    bag: persistedReducer,
   },
 });
 
